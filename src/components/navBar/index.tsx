fef90d6f0cfbff422a946f4fe1305fbf38737176
AlexisRqs <alexisrqs@outlook.fr>
2023-03-31 12:19:17 +0200

    Working nav bar (but ugly af)

--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -1,9 +1,18 @@
 import * as React from 'react'
+import {Link} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <div>
-
+            <Link to = "/">
+                Accueil
+            </Link>
+            <Link to = "/categories">
+                Catégories
+            </Link>
+            <Link to = "/movies/:id">
+                Films
+            </Link>
         </div>
     )
 }
