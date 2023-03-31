15a13676959c38e8482d2e595157383f53146fdb
Maxime2023 <maximedizier06500@gmail.com>
2023-03-31 10:55:13 +0200

    adding .env

--- src/components/pages/HomePage/index.tsx
+++ src/components/pages/HomePage/index.tsx
@@ -1,10 +1,12 @@
 import { Box, Typography } from '@mui/material';
 
 const HomePage = () => {
+  const apiUrl = process.env.REACT_APP_TMDB_API;
+  const apiKey = process.env.REACT_APP_TMDB_API_APIKEY;
   return (
     <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
       <Typography>
-        Home Page
+        Home Page {apiUrl} {apiKey}
       </Typography>
     </Box>
   );
